6b304fe202e7ea52926dfeecbcfa26cd96e62693
jeethesh <pjithesh06@gmail.com>
2023-07-02 23:27:47 +0200

    vscode settings added

--- Chapter01/01_cuda_introduction/01_hello_world/hello_world.cu
+++ Chapter01/01_cuda_introduction/01_hello_world/hello_world.cu
@@ -1,15 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-__global__ void print_from_gpu(void) {
+__global__ void print_from_gpu(void)
+{
 	printf("Hello World! from thread [%d,%d] \
-		From device\n", threadIdx.x,blockIdx.x); 
+		From device\n",
+		   threadIdx.x, blockIdx.x);
 }
 
-int main(void) { 
-	printf("Hello World from host!\n"); 
-	print_from_gpu<<<1,1>>>();
+int main(void)
+{
+	printf("Hello World from host!\n");
+	print_from_gpu<<<2, 1>>>();
 	cudaDeviceSynchronize();
-return 0; 
+	return 0;
 }
-
